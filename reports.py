566f8bc695d42aaae53021600bd241ca499444f0
iulian-coder <iulian.titei@gmail.com>
2019-11-08 20:16:12 +0200

    test get_latest

--- reports.py
+++ reports.py
@@ -1,2 +1,32 @@
+# Open file
+def get_tabel(file_name):
+    tabel = []
+    with open(file_name, "r") as file:
+        for line in file:
+            colums = line.split('\t')
+            tabel.append(colums)
+    return tabel
 
-# Report functions
+
+# How many games are in the file
+def count_games(file_name):
+    return len(get_tabel(file_name))
+
+
+# Is there a game from a given year?
+def decide(file_name, year):
+    for row in get_tabel(file_name):
+        if int(row[2]) == year:
+            return True
+    return False
+
+def get_latest(file_name):
+    max_year = get_latest(file_name)[0][2]
+    
+
+
+
+
+
+
+
