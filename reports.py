94c5ace66bb4f1d70f53a843e907f7a57aea4e70
iulian-coder <iulian.titei@gmail.com>
2019-11-08 22:15:51 +0200

    bonu to prepear

--- reports.py
+++ reports.py
@@ -3,8 +3,15 @@ def get_tabel(file_name):
     tabel = []
     with open(file_name, "r") as file:
         for line in file:
-            colums = line.split('\t')
-            tabel.append(colums)
+            columns = line.split('\t')
+            columns_names = {
+                "title": columns[0],
+                "copies": columns[1],
+                "year": columns[2],
+                "genre": columns[3],
+                "publisher": columns[4]
+            }
+            tabel.append(columns_names)
     return tabel
 
 
@@ -16,17 +23,42 @@ def count_games(file_name):
 # Is there a game from a given year?
 def decide(file_name, year):
     for row in get_tabel(file_name):
-        if int(row[2]) == year:
+        if int(row["year"]) == year:
             return True
     return False
 
+
+# Which was the latest game?
 def get_latest(file_name):
-    max_year = get_latest(file_name)[0][2]
-    
+    max_year = get_tabel(file_name)[0]["year"]
+    latest_title = None
+    for row in get_tabel(file_name):
+        actual_year = row["year"]
+        if actual_year > max_year:
+            max_year = actual_year
+            latest_title = row["title"]
+    return latest_title
+
+def count_by_genre(file_name, genre):
+    counter = 0
+    for row in get_tabel(file_name):
+        if row["genre"] == genre:
+            counter += 1
+    return counter
 
 
+def get_line_number_by_title(file_name, title):
+    counter = 0
+    for row in get_tabel(file_name):
+        counter += 1
+    if row['title'] == title:
+        return counter
+    raise ValueError("No such game!")
 
-
-
+# Bonus Function
+def sort_abc(file_name):
+    tabel = get_tabel(file_name)
+    for row in tabel:
+        for i in range(len(tabel - 1)
 
 
